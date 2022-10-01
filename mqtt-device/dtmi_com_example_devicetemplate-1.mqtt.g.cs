16a580010822278a51d143f5874daeb29a811a23
Rido <ridomin@live.com>
2022-09-30 18:09:11 -0700

    Update to use 0.4 (#2)
    * add docker file
    
    * update to 0.3
    
    * update to 0.4
    
    * init properties
    
    * clean up
    
    * readme updates
    
    * clean csproj

--- mqtt-device/dtmi_com_example_devicetemplate-1.mqtt.g.cs
+++ mqtt-device/dtmi_com_example_devicetemplate-1.mqtt.g.cs
@@ -3,25 +3,23 @@
 using MQTTnet.Client;
 using MQTTnet.Extensions.MultiCloud;
 using MQTTnet.Extensions.MultiCloud.BrokerIoTClient;
-using MQTTnet.Extensions.MultiCloud.BrokerIoTClient.TopicBindings;
-
 namespace dtmi_com_example_devicetemplate.mqtt;
 
-public class devicetemplate : PnPMqttClient, Idevicetemplate
+public class devicetemplate : Idevicetemplate
 {
+    public IMqttClient Connection { get; set; }
+    public string InitialState { get; set; }
     public IReadOnlyProperty<string> Property_sdkInfo { get; set; }
     public IWritableProperty<int> Property_interval { get; set; }
     public ITelemetry<double> Telemetry_temp { get; set; }
-    public ICommand<Cmd_echo_Request, Cmd_echo_Response> Command_echo { get; set; }
+    public ICommand<string, string> Command_echo { get; set; }
 
-    public devicetemplate(IMqttClient c) : base(c, Idevicetemplate.ModelId)
+    public devicetemplate(IMqttClient c) 
     {
+        Connection = c;
         Property_sdkInfo = new ReadOnlyProperty<string>(c, "sdkInfo");
         Property_interval = new WritableProperty<int>(c, "interval");
         Telemetry_temp = new Telemetry<double>(c, "temp");
-        Command_echo = new Command<Cmd_echo_Request, Cmd_echo_Response>(c, "echo");
+        Command_echo = new Command<string, string>(c, "echo");
     }
-
-    public async Task<MqttClientPublishResult> SendTelemetryAsync(AllTelemetries payload, CancellationToken t) =>
-        await base.SendTelemetryAsync(payload, t);
 }
